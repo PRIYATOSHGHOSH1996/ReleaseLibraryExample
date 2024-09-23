a0cac541ec71f49687a8b59cf10f0dc4366dca50
PRIYATOSH <47550482+PRIYATOSHGHOSH1996@users.noreply.github.com>
2024-09-23 15:28:15 +0530

    add

--- releaseLibrary/build.gradle.kts
+++ releaseLibrary/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate{
             register<MavenPublication>("release"){
                 groupId = "com.priyatosh"
                 artifactId = "releaselibrary"
-                version = "1.0.0"
+                version = "1.0.1"
                 from(components["release"])
             }
         }
