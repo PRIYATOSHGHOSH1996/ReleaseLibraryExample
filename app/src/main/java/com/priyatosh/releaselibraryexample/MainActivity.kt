9e692512a39adf48f302ce70b6217be5bbd23319
PRIYATOSH <47550482+PRIYATOSHGHOSH1996@users.noreply.github.com>
2024-09-23 14:41:04 +0530

    add

---
+++ app/src/main/java/com/priyatosh/releaselibraryexample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.priyatosh.releaselibraryexample
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.priyatosh.releaselibrary.CommonClass
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<TextView>(R.id.text).text = "Sum of 2 and 3 is ${CommonClass.sumOfTwoNumber(2,3)}"
+    }
+}
---
+++ releaseLibrary/src/main/java/com/priyatosh/releaselibrary/CommonClass.kt
@@ -0,0 +1,5 @@
+package com.priyatosh.releaselibrary
+
+object CommonClass {
+    fun sumOfTwoNumber(a:Int,b:Int) = a+b
+}
